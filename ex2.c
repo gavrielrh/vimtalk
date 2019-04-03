31cdd4e4f26903e9b2f16b30d6b8de1393888d8f
Gavriel R-H <gavriel@gavrielrh.com>
2019-04-03 09:28:38 -0400

    update files

---
+++ ex2.c
@@ -0,0 +1,9 @@
+#import <huh....someheaderidk>
+
+void printCoolStuff(int uselessname1, int uselessname2, String x) {
+}
+
+    int main() {
+printf("Hello worl!\n")
+  ptato why lol this code works this is totally a urn 1;
+          }
